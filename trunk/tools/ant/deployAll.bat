4da3d77690ff1c7354667a748a5148c0484e705b
Ravi <howthinkers@gmail.com>
2011-11-20 18:26:48 -0600

    add proj

--- trunk/tools/ant/deployAll.bat
+++
@@ -1,9 +0,0 @@
-
-set ORACLE_HOME=C:\oracle\MiddlewareJdev11gR1
-set ANT_HOME=%ORACLE_HOME%\jdeveloper\ant
-set PATH=%ANT_HOME%\bin;%PATH%
-set JAVA_HOME=%ORACLE_HOME%\jdk160_11
-
-set CURRENT_FOLDER=%CD%
-
-ant -f build.xml deployAll -Dbasedir=%ORACLE_HOME%\jdeveloper\bin
